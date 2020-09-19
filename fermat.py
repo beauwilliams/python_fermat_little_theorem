24139377498267e3c1ca26ca781565c23f58a9ad
beauwilliams <beau.williams@icloud.com>
2020-09-19 21:19:19 +1000

    Created

--- fermat.py
+++ fermat.py
@@ -1,6 +1,8 @@
 import sys
 
 print("Input two numbers to test if they are relatively coprime using Fermats Little Theorem")
+print("Try the numbers 1763 & 86 for example and see what output we get!")
+print("Ask yourself, why might that be?")
 a_in = input ("Enter a number for a: ")
 p_in = input ("Enter a number for p: ")
 
@@ -13,7 +15,13 @@ def gcd(a, b):
         a, b = b, a%b
     return a
 
+result = (a**(p-1)) % p
 print ("a=",a)
 print ("p=",p)
 print ("GCD(a,p)=",gcd(a,p))
-print ("Result: ",(a**(p-1)) % p)
+print ("Result of FMT: ",(a**(p-1)) % p)
+
+if result == 1:
+    print("Relatively Comprime")
+elif result != 1:
+    print("NOT Relatively Comprime")
